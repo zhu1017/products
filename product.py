cda08d2429fb036f405780be950f3349d412e9ab
zhu1017 <hs610107@hmjh.chc.edu.tw>
2022-05-02 10:26:27 +0800

    n

---
+++ product.py
@@ -0,0 +1,17 @@
+produsts = []
+while True:
+    name = input('請輸入商品名稱:')
+    if name == 'q' :
+        break
+    price = input('請輸入商品價格:')
+    price = int(price)
+    produsts.append([name])
+print(produsts)
+
+for p in produst:
+	print(p[0]，'的價格是'，p[1])
+
+with open('produsts.csv'，'w',encoding ='utf-8')as f:
+	f.write('商品，價格\n')
+	for p in produsts:
+		f.write(p[0] + ',' + str(p[1]) + '\n')
