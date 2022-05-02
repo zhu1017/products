7369baa40221579cbe4125a77dfa6695c5c5ce54
zhu1017 <hs610107@hmjh.chc.edu.tw>
2022-05-02 10:44:08 +0800

    ne

--- product.py
+++ product.py
@@ -5,13 +5,14 @@ while True:
         break
     price = input('請輸入商品價格:')
     price = int(price)
-    produsts.append([name])
+    p = []
+    produsts.append([name,price])
 print(produsts)
 
 for p in produst:
-	print(p[0]，'的價格是'，p[1])
+	print(p[0],'的價格是',p[1])
 
-with open('produsts.csv'，'w',encoding ='utf-8')as f:
+with open('produsts.csv','w',encoding ='utf-8')as f:
 	f.write('商品，價格\n')
 	for p in produsts:
 		f.write(p[0] + ',' + str(p[1]) + '\n')
